d9415248664d9f6b9db63e8c26b5d8fe82f18b98
Alexis Zambrano <alexis.zambrano@techtonic.com>
2018-10-08 09:44:49 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//problem-1
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-1").innerHTML =
+
+
+//problem-2
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-2").innerHTML =
+
+//problem-3
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-3").innerHTML =
+
+
+//problem-4
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-4").innerHTML =
+
+
+//challenge-1
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("challenge-1").innerHTML =
+
+
+//challenge-2
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("challenge-2").innerHTML =
